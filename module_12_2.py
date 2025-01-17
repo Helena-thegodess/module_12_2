cb6efe81b079a93ce12dba51d03d2aecd1a5b802
Elena <helena1990.18.12@gmail.com>
2025-01-18 00:15:10 +0300

    Update module_12_2.py

--- module_12_2.py
+++ module_12_2.py
@@ -14,26 +14,32 @@ class TournamentTest(unittest.TestCase):
 
     @classmethod
     def tearDownClass(cls):
-        for key, value in enumerate(cls.all_results):
-            print(f"{key + 1}:{value}")
+        res = {}
+        for key, value in cls.all_results.items():
+            for k, v in value.items():
+                res[k] = str(v)
+            print(res)
 
     def test1(self):
         marathon_1 = runner_and_tournament.Tournament(90, self.runner1, self.runner3)
-        results = marathon_1.start()
-        TournamentTest.all_results.append(results)
-        self.assertTrue(results[2] == "Ник")
+        result = marathon_1.start()
+        last_runner = list(result.values())
+        self.assertTrue(last_runner[-1] == 'Ник')
+        self.all_results[result.values()] = result
 
     def test2(self):
         marathon_2 = runner_and_tournament.Tournament(90, self.runner2, self.runner3)
-        results = marathon_2.start()
-        TournamentTest.all_results.append(results)
-        self.assertTrue(results[2] == "Ник")
+        result = marathon_2.start()
+        last_runner = list(result.values())
+        self.assertTrue(last_runner[-1] == 'Ник')
+        self.all_results[result.values()] = result
 
     def test3(self):
         marathon_3 = runner_and_tournament.Tournament(90, self.runner1, self.runner2, self.runner3)
-        results = marathon_3.start()
-        TournamentTest.all_results.append(results)
-        self.assertTrue(results[3] == "Ник")
+        result = marathon_3.start()
+        last_runner = list(result.values())
+        self.assertTrue(last_runner[-1] == 'Ник')
+        self.all_results[result.values()] = result
 
 if __name__ == "__main__":
     unittest.main()
